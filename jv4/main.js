a78d3496feeff215c4cf7be3e76b8709ce28c009
dangphuc282 <dangphuc28022002@gmail.com>
2025-05-27 14:19:36 +0900

    Add files via upload

---
+++ jv4/main.js
@@ -0,0 +1,3 @@
+let firstNumber = Number(prompt('Nhập vào : firstNumber: '));
+let secondNumber = Number(prompt('Nhập vào : secondNumber'));
+console.log('Tổng 2 số là: ' + (firstNumber + secondNumber));
