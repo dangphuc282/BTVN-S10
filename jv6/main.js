5a70cbff3298159f744565b86e56c66054c1d8bc
dangphuc282 <dangphuc28022002@gmail.com>
2025-05-27 14:44:15 +0900

    Add files via upload

---
+++ jv6/main.js
@@ -0,0 +1,5 @@
+let math = Number(prompt('Nhập điểm toán'));
+let physics = Number(prompt('Nhập điểm vật lý'));
+let chemistry = Number(prompt('Nhập điểm hoá hoc'));
+let sum = math + physics + chemistry
+console.log(`điểm trung bình 3 môn của bạn là ` + (sum/3) + ` điểm`);
